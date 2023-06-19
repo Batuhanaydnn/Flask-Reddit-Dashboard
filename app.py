b1a996304e84dd6ce104f913d31708de4d92e320
Batuhanaydnn <code.batuhanaydin@gmail.com>
2023-06-19 22:53:52 +0300

    User model has been prepared and register field has been added

--- app.py
+++ app.py
@@ -2,13 +2,26 @@ from flask import Flask, render_template, request, session, redirect, url_for, j
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import praw
+from models import db, User
 
 app = Flask(__name__)
 app.app_context().push()
 
+# Home page
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# Register Page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        user = User(name=name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,18 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app.secret_key = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+# Define User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
