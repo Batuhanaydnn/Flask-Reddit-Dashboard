c964d755c3c45597707d0dcbaa998d17ed8f7380
Batuhanaydnn <code.batuhanaydin@gmail.com>
2023-06-20 03:43:39 +0300

    API prepared, Reddit-Post ready, templates completed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, session, redirect, url_for, j
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import praw
-from models import db, User
+from models import db, User, Post
 
 app = Flask(__name__)
 app.app_context().push()
@@ -36,6 +36,44 @@ def login():
         else:
             return render_template('login.html', message='Invalid email or password')
     return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' in session:
+        user = User.query.filter_by(id=session['user_id']).first()
+
+        # Connect to Reddit API
+        reddit = praw.Reddit(client_id='JMqEtE1wdrD39jhY_ZwTQA',
+                             client_secret='dDqqLlBcblEFzj9EylLgckbPIHdz6w',
+                             user_agent='technicalqualifierwasveryeasy')
+        subreddit = reddit.subreddit('netsec')
+
+        for post in subreddit.new(limit=10):
+            if not db.session.query(Post).filter(Post.title == post.title).first():
+                new_post = Post(title=post.title, content=post.selftext, user=user)
+                db.session.add(new_post)
+                db.session.commit()
+
+        posts = Post.query.all()
+
+        if request.headers.get('accept') == 'application/json':
+            return jsonify(posts=[post.__dict__ for post in posts])
+        else:
+            return render_template('dashboard.html', user=user, posts=posts)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/api/posts/')
+def api_posts():
+    posts = Post.query.all()
+    posts_list = []
+    for post in posts:
+        post_dict = {}
+        for key, value in post.__dict__.items():
+            if key != '_sa_instance_state':
+                post_dict[key] = value
+        posts_list.append(post_dict)
+    return jsonify(posts=posts_list)
 
 @app.route('/logout')
 def logout():
--- models.py
+++ models.py
@@ -16,3 +16,15 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.name
+
+# Define Post model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
